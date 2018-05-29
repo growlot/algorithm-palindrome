50cbc24753d9b6675b5296dea26068e4e55f3a68
growlot <lovemobileweb227@aol.com>
2018-05-30 02:26:16 +0300

    Initial algorithm

---
+++ DerivePalindrome/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DerivePalindrome
+{
+    class Program
+    {
+        static string DerivePalindrome(string input)
+        {
+            var output = input;
+            for (int i = 0; i < output.Length / 2; i++)
+            {
+                if (output[i] != output[output.Length - 1 - i])
+                {
+                    output = output.Insert(output.Length - i, output[i].ToString());
+                }
+            }
+            return output;
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: app.exe <word>");
+                return;
+            }
+            foreach (var input in args)
+            {                
+                Console.WriteLine($@"{input} => {DerivePalindrome(input)}");
+            }
+        }
+    }
+}
